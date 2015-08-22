1fff61f726fabec0013acb3a347d126285950635
navintellis <navintellis@gmail.com>
2015-08-22 13:22:00 +0530

    test

--- www/js/services.js
+++ www/js/services.js
@@ -8,7 +8,7 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var friends = [
-    { id: 0, name: 'Scruff McGruff' },
+    { id: 0, name: 'Scruff McGrufff' },
     { id: 1, name: 'G.I. Joe' },
     { id: 2, name: 'Miss Frizzle' },
     { id: 3, name: 'Ash Ketchum' },
